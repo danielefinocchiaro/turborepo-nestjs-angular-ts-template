00a5e0290b77facdb10db7b20ec0172f31531727
“danielefinocchiaro” <danielefinocchiaro@keix.com>
2024-08-25 15:48:22 +0200

    fixed export of prisma

--- apps/backend/src/users/users.service.ts
+++ apps/backend/src/users/users.service.ts
@@ -1,29 +1,29 @@
 import { Injectable } from "@nestjs/common";
-import type { User } from "prisma";
+import type { User } from "prisma-glob";
 // biome-ignore lint/style/useImportType: This is a bug in the ESLint plugin for Nx
 import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
 export class UsersService {
-	constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
-	create(createUserDto: User) {
-		return "This action adds a new user";
-	}
+  create(createUserDto: User) {
+    return "This action adds a new user";
+  }
 
-	findAll() {
-		return this.prisma.user.findMany();
-	}
+  findAll() {
+    return this.prisma.user.findMany();
+  }
 
-	findOne(id: number) {
-		return `This action returns a #${id} user`;
-	}
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
 
-	update(id: number, updateUserDto: Partial<User>) {
-		return `This action updates a #${id} user`;
-	}
+  update(id: number, updateUserDto: Partial<User>) {
+    return `This action updates a #${id} user`;
+  }
 
-	remove(id: number) {
-		return `This action removes a #${id} user`;
-	}
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
 }
--- apps/frontend/src/app/services/utils.service.ts
+++ apps/frontend/src/app/services/utils.service.ts
@@ -1,19 +1,19 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import type { User } from "prisma";
+import type { User } from "prisma-glob";
 
 @Injectable({
-	providedIn: "root",
+  providedIn: "root",
 })
 export class UtilsService {
-	private API_URL = "http://localhost:3333/api";
+  private API_URL = "http://localhost:3333/api";
 
-	constructor(private http: HttpClient) {
-		// This service can now make HTTP requests via `this.http`.
-	}
+  constructor(private http: HttpClient) {
+    // This service can now make HTTP requests via `this.http`.
+  }
 
-	getUsers(): Observable<User[]> {
-		return this.http.get<User[]>(`${this.API_URL}/users`);
-	}
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.API_URL}/users`);
+  }
 }
